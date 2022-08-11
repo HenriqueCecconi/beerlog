c9511d2c1ef18f795ab31c2b3fe39d1cf1c3e1d8
Henrique Cecconi <henrique.cecconi@gmail.com>
2022-08-11 19:26:56 -0300

    Basic modeling of my data in the 'Beer' class

---
+++ models.py
@@ -0,0 +1,11 @@
+from typing import Optional
+from sqlmodel import SQLModel, Field
+from sqlmodel import select
+
+class Beer(SQLModel, table=True):
+    id: Optional[int] = Field(primary_key=True, default=None)
+    name: str
+    style: str
+    flavor: int
+    image: int
+    cost: int
