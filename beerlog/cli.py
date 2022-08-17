750839ab9a3c187b0ba2a44af458bb5225d291b2
Henrique Cecconi <henrique.cecconi@gmail.com>
2022-08-16 21:44:29 -0300

    Creating a basic template for my cli application

--- beerlog/cli.py
+++ beerlog/cli.py
@@ -1,4 +1,19 @@
-from beerlog.config import settings
+import typer
 
-def main():
-    print('Hello from', settings.NAME)
+main = typer.Typer(help='Beer Managment Application')
+
+@main.command('add')
+def add(
+    name: str,
+    style: str,
+    flavor: int = typer.Option(...),
+    image: int  = typer.Option(...),
+    cost: int   = typer.Option(...),
+):
+    """Adds new beer to database."""
+    print(name, style)
+
+@main.command('list')
+def list_beers(style: str):
+    """Lists the beers currently in database"""
+    print(style)
